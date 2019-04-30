c0c9d8cc0ee424d03dc31d8060bdfc09eaa4c48a
pal_user <eric.gentry045@myci.csuci.edu>
2019-04-30 16:37:35 -0700

    Adding circuit-breaker to the registration service

---
+++ applications/eureka-server/src/main/java/io/pivotal/pal/tracker/eureka/App.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.util.TimeZone;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(App.class, args);
+    }
+}
